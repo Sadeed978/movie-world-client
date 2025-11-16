011bbd2b62316e559b4a127453f4e612e13dfcd6
Sadeed978 <sadeedahmed681@gmail.com>
2025-11-16 10:22:59 +0600

    added the error page

--- src/Pages/Errorpage.jsx
+++ src/Pages/Errorpage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
-const Errorpage = () => {
+import imageError from '../assets/error-404.png';
+const ErrorPage = () => {
     return (
-        <div>
-            404
-            
+        <div className='flex flex-col justify-center items-center'>
+            <img src={imageError} alt="" />
+            <h1 className='text-5xl text-center mt-20'>404 - Page Not Found</h1>
         </div>
     );
 };
 
-export default Errorpage;
+export default ErrorPage;
