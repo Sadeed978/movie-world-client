0ae5bc3a4eb8c7878b6df807119671e5e57b42ff
Sadeed978 <sadeedahmed681@gmail.com>
2025-11-16 20:52:15 +0600

    correction the footer part

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
-
+import { FaYoutube } from "react-icons/fa";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaFacebookF } from "react-icons/fa6";
 const Footer = () => {
     return (
         <div>
-            <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-                <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
-                </nav>
-                <nav>
-                    <h6 className="footer-title">Company</h6>
+            <footer className="footer footer-horizontal footer-center p-4 bg-black text-white">
+                <nav className="grid grid-flow-col gap-4">
                     <a className="link link-hover">About us</a>
                     <a className="link link-hover">Contact</a>
                     <a className="link link-hover">Jobs</a>
                     <a className="link link-hover">Press kit</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <div className="grid grid-flow-col gap-4">
+                        <a>
+                        <RiTwitterXFill />
+                        </a>
+                        <a>
+                        <FaYoutube />
+                        </a>
+                        <a>
+                        <FaFacebookF />
+                        </a>
+                    </div>
                 </nav>
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+                </aside>
             </footer>
         </div>
     );
